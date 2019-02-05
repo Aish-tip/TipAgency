8e619345dead7ae114a4ff2604e0ecb1761d2673
Aish-tip <akaishu1995@gmail.com>
2019-02-05 11:46:59 +0530

    component and routing is made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CapabilitiesComponent } from './pages/capabilities/capabilities.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { GamingComponent } from './pages/gaming/gaming.component';
+import { TeamComponent } from './pages/team/team.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CareerComponent } from './pages/career/career.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {path:'', component:HomeComponent},
+    {path:'capabilities', component:CapabilitiesComponent},
+    {path:'portfolio', component:PortfolioComponent},
+    {path:'clients', component:ClientsComponent},
+    {path:'gaming', component:GamingComponent},
+    {path:'team', component:TeamComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'career', component:CareerComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CapabilitiesComponent } from './pages/capabilities/capabilities.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ClientsComponent } from './pages/clients/clients.component';
+import { GamingComponent } from './pages/gaming/gaming.component';
+import { TeamComponent } from './pages/team/team.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CareerComponent } from './pages/career/career.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CapabilitiesComponent,
+    PortfolioComponent,
+    ClientsComponent,
+    GamingComponent,
+    TeamComponent,
+    ContactComponent,
+    CareerComponent
   ],
   imports: [
     BrowserModule,
